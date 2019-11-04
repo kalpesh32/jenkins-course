e56ce24305c61567572ca59edb97251db38aa691
kalpesh32 <36621872+kalpesh32@users.noreply.github.com>
2019-11-04 16:28:13 +0530

    save ID docker credential

--- job-dsl/nodejsdocker.groovy
+++ job-dsl/nodejsdocker.groovy
@@ -16,7 +16,7 @@ job('NodeJS Docker kalpesh-test') {
         dockerBuildAndPublish {
             repositoryName('kalpesh32/docker-nodejs-demo')
             tag('${GIT_REVISION,length=9}')
-            registryCredentials('kalpesh32')
+            registryCredentials('8d052f15-d987-4b0b-a7c5-c374b21e8e61')
             forcePull(false)
             forceTag(false)
             createFingerprints(false)
